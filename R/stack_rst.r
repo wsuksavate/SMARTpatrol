fc074dc69fcbde235a7334088df9eaad305d2cc8
Warong Suksavate <wsuksavate@gmail.com>
2022-04-06 10:37:15 +0700

    second test

--- R/stack_rst.r
+++ R/stack_rst.r
@@ -1,3 +1,13 @@
+#' stack raster function
+#'
+#' This function allows you to extract the las element of a vector.
+#' @param v A vector.
+#' @keywords last
+#' @export
+#' @examples
+#' y<-rnorm(10,0,1)
+#' stack(r.list, rst)
+
 stack_rst <- function(r.list, rst){
 	r.pool <- lapply(r.list, FUN=function(x) raster::raster(x) %>% raster::reclassify(cbind(-Inf, NA, 0)) %>% raster::resample(pta))
 	r.stack <- raster::stack(r.pool)
